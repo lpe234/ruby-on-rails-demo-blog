284be0d067d160628541d77f4a9e00f6d95a6cb0
lpe234 <lpe234@qq.com>
2017-09-18 13:53:58 +0800

    model validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
-
+  validates :title, :content, :presence => true
+  validates :title, :length => { :minimum => 2 }
+  validates :title, :uniqueness => { :message => 'Already taken' }
 end
